d4a451b6c43c4b693bc3640d2f648ae2927df55c
zsylj <34911816+zsylj@users.noreply.github.com>
2021-09-24 02:30:48 +0800

    Update [G3]entity_spec_mod.py

--- [1]Axis-blitzkrieg.usc/[G3]entity_spec_mod.py
+++ [1]Axis-blitzkrieg.usc/[G3]entity_spec_mod.py
@@ -65,7 +65,7 @@ def UoC2(uoc_dir):
 
     print("Final")
     print(spec_list)
-UoC2(r"F:\Steam\steamapps\common\Unity of Command 2\_packages\dlc1\data\campaigns\blitzkrieg.usc\scenarios")
+UoC2(r"_packages\dlc1\data\campaigns\blitzkrieg.usc\scenarios")
 
 
 def spec_mod(path,spec_list):
@@ -87,4 +87,4 @@ def spec_mod(path,spec_list):
             yaml.dump(temp,f)
         f.closed
 
-spec_mod(r"F:/Steam/steamapps/common/Unity of Command 2/_packages/base/data/entity_types/specialists/",spec_list)
+spec_mod(r"_packages/base/data/entity_types/specialists/",spec_list)
