80dadff60642d1bd7ef3e330b79e6e6f70cfe641
zsylj <34911816+zsylj@users.noreply.github.com>
2021-09-23 08:47:56 +0800

    Add files via upload

---
+++ [G4]entity_unit_mod.py
@@ -0,0 +1,84 @@
+import os
+import re
+import yaml
+
+#Look up table
+def lut(path):
+    f=open(path,"r")
+    temp_all=f.read()
+    temp_list=temp_all.split("\n")
+    pair_list=[item for item in temp_list if "name" not in item]
+    del pair_list[-1]
+
+    #读取本DLC中所有涉及到的Unit ID
+    id_list=[pair_list[i][6::] for i in range(0,len(pair_list),2)]
+    #print(len(id_list))
+    #print(id_list)
+
+    #读取本DLC中所有涉及到的Unit Type
+    type_list=[pair_list[i][8::] for i in range(1,len(pair_list),2)]
+    #print(len(type_list))
+    #print(type_list)
+
+    #对Unit Type的前部分国家代码进行记录
+    country_list=[]
+    for item in type_list:
+        ind=item.find("_")#头部国家代码定位
+        country_list.append(item[0:ind])
+    #print(country_list)
+
+    #将Unit Type进行阵营分割
+    #红色阵营、盟军阵营、轴心阵营
+    faction_list=[]
+    red_fac=["sov"]
+    allied_fac=["us","bra","ff","uk","aus","can","gr","ind","nor","nz","pol","sa","yug","bel","fr","nl","swe","sws"]
+    axis_fac=["ger","ss","cro","hun","ita","rom","rsi","svk","vf","bul","est","fin","spa"]
+    for item in country_list:
+        if item in red_fac:
+            faction_list.append("red")
+        elif item in allied_fac:
+            faction_list.append("allied")
+        else:
+            faction_list.append("axis")
+    #print(faction_list)
+
+    #去重复的Unit Type列表
+    rr_type_list=[]
+    for item in type_list:
+        ind=item.find("_")
+        if item[0:ind] in allied_fac:##############
+            if item in rr_type_list:
+                continue
+            else:
+                rr_type_list.append(item)
+        else:
+            continue
+    print(rr_type_list)
+        
+    f.closed
+    return id_list,country_list,faction_list,rr_type_list
+
+id_list,country_list,faction_list,rr_type_list=lut(r"F:/Steam/steamapps/common/Unity of Command 2/_packages/base/data/campaigns/tutorial.usc/units.yml")#################
+
+#
+def unit_mod(path,rr_type_list):
+    for item in rr_type_list:
+        new_path=path+item+".yml"
+        f=open(new_path,"r")
+        unit_content=f.read()
+        temp=yaml.safe_load(unit_content)
+        temp["move"]=100
+        temp["xmove"]=100
+        temp["attack"]=100
+        temp["defense"]=100
+        temp["armor"]=100
+        temp["max_st"]=7
+        temp["max_spec"]=3
+        #print(temp)
+        f.closed
+        with open(new_path,"w") as f:
+            yaml.dump(temp,f)
+        f.closed
+
+#unit_mod(r"D:/Program Files/Steam/steamapps/common/Unity of Command 2/_packages/base/data/entity_types/unit_types/",rr_type_list)
+unit_mod(r"F:/Steam/steamapps/common/Unity of Command 2/_packages/base/data/entity_types/unit_types/",rr_type_list)
---
+++ [LS4]bridge_and_fort_modification.py
@@ -0,0 +1,59 @@
+import os
+import re
+import yaml
+
+def bridge_mod(path):
+    f=open(path,"r")
+    bridge_content=f.read()
+    temp=yaml.safe_load(bridge_content)
+    try:
+        del temp["map"]
+    except KeyError:
+        print("Bridges.yml Do Not Contain Key['map'] i.e. This scene do not have broken bridge")
+    try:
+        for item in temp["gamelike"]:
+            #print(item)
+            item[1]["in_build"]=False
+    except KeyError:
+        print("Bridges.yml Do Not Contain Key['gamelike'] i.e. This scene do not have floating bridge")
+    f.closed
+    with open(path,"w") as f:
+        yaml.dump(temp,f)
+    f.closed
+
+def fort_mod(path):
+    f=open(path,"r")
+    fort_content=f.read()
+    temp=yaml.safe_load(fort_content)
+    #print(temp)
+    #print("=======================================")
+    for item in temp["map"]:
+        item[1]["functional"]=False
+        #print(item)
+    f.closed
+    #print(temp)
+    with open(path,"w") as f:
+        yaml.dump(temp,f)
+    f.closed
+
+def UoC2(uoc_dir):
+    scene_name_list=os.listdir(uoc_dir)
+    for scene_name in scene_name_list:
+        scene_path=os.path.join(uoc_dir,scene_name)
+        print(scene_path)
+        #若本文件夹内还有文件夹，调用函数
+        if os.path.isdir(scene_path)==True:
+            UoC2(scene_path)
+        else:
+            if r"\bridges.yml" in scene_path:
+
+                print("F1")
+                bridge_mod(scene_path)
+                print("END")
+                
+            elif r"\forts.yml" in scene_path:
+                print("F2")
+                fort_mod(scene_path)
+                print("END")
+
+temp=UoC2(r"F:\Steam\steamapps\common\Unity of Command 2\_packages\base\data\campaigns\tutorial.usc\scenarios")
