3124ca5a7c428a01d7178b32942ad935aefad8fd
zsylj <34911816+zsylj@users.noreply.github.com>
2021-09-24 05:02:32 +0800

    Update [G2]entity_hq_mod#.py

--- [1]Axis-blitzkrieg.usc/[G2]entity_hq_mod#.py
+++ [1]Axis-blitzkrieg.usc/[G2]entity_hq_mod#.py
@@ -35,57 +35,54 @@ def UoC2(uoc_dir):
             UoC2(scene_path)
         else:
             if r"\hqs.yml" in scene_path:
-
                 print("F1")
                 hq(scene_path,hq_list)
                 print("END")
-
     print("Final")
     print(hq_list)
 UoC2(r"_packages\dlc1\data\campaigns\blitzkrieg.usc\scenarios")
 
 def hq_mod(path,hq_list):
-    difficulties_list=["easy","normal","classic","hard"]
-    history_intervals_list=["early","mid","late"]
-    
     for item in hq_list:
-        if item!="ita_hq":
-            new_path=path+item+".yml"
-            f=open(new_path,"r")
-            hq_content=f.read()
-            temp=yaml.safe_load(hq_content)
-            print(temp)
-            for item2 in temp["branches"]:
-                print(item2)
-                for item3 in temp["branches"][item2]:
-                    print(item3)
-                    item3["cost"]=1
-            temp["fp_max_steps"]=1000000
-            temp["max_cp"]=9
-            temp["trucks"]=5
-            #个别hq没有init_levels键
-            try:
-                for item4 in temp["init_levels"]:
-                    temp["init_levels"][item4]=3
-            except KeyError:
-                temp.update({'init_levels': {'engineering': 3, 'force_pool': 3, 'intel': 3, 'logistics': 3, 'operations': 3}})
-            #个别hq没有fixed_movement键
-            try:
-                temp["fixed_movement"]=100#
-            except KeyError:
-                temp.update({'fixed_movement': 100})
-            #个别hq没有fixed_range键
-            try:
-                temp["fixed_range"]=100#
-            except KeyError:
-                temp.update({'fixed_range': 100})
-            f.closed
-            
+        new_path=path+item+".yml"
+        f=open(new_path,"r")
+        hq_content=f.read()
+        temp=yaml.safe_load(hq_content)
+        print(temp)
+        for item2 in temp["branches"]:
+            print(item2)
+            for item3 in temp["branches"][item2]:
+                print(item3)
+                item3["cost"]=1
+        temp["fp_max_steps"]=1000000
+        temp["max_cp"]=9
+        temp["trucks"]=5
+        #个别hq没有init_levels键
+        try:
+            for item4 in temp["init_levels"]:
+                temp["init_levels"][item4]=3
+        except KeyError:
+            temp.update({'init_levels': {'engineering': 3, 'force_pool': 3, 'intel': 3, 'logistics': 3, 'operations': 3}})
+        #个别hq没有fixed_movement键
+        try:
+            temp["fixed_movement"]=100#
+        except KeyError:
+            temp.update({'fixed_movement': 100})
+        #个别hq没有fixed_range键
+        try:
+            temp["fixed_range"]=100#
+        except KeyError:
+            temp.update({'fixed_range': 100})
+        f.closed
+        
+        hq_check_list=["can_hq","ff_hq","ger_hq","hun_hq","ita_hq","uk_hq","us_hq","vichy_hq"]
+        if item not in hq_check_list:
             with open(new_path,"w") as f:
                 yaml.dump(temp,f)
             f.closed
-            
         else:
+            difficulties_list=["easy","normal","classic","hard"]
+            history_intervals_list=["early","mid","late"]
             for item_hist in history_intervals_list:
                 for item_diff in difficulties_list:
                     new_path2=path+item+"_"+item_hist+"_"+item_diff+".yml"
